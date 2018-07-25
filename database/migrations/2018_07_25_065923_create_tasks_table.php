5d7071d5bd82b6d3c72acf463d55dc333bc49c44
Marius <marius.kaziukevicius@gmail.com>
2018-07-25 19:04:01 +0300

    change tasks schema description to nullable

--- database/migrations/2018_07_25_065923_create_tasks_table.php
+++ database/migrations/2018_07_25_065923_create_tasks_table.php
@@ -17,7 +17,7 @@ class CreateTasksTable extends Migration
             $table->increments('id');
             $table->unsignedInteger('owner_id');
             $table->string('title')->collation('utf8_unicode_ci');
-            $table->text('description')->collation('utf8_unicode_ci');
+            $table->text('description')->collation('utf8_unicode_ci')->nullable();
             $table->dateTime('due_date')->nullable();
             $table->dateTime('completed_date')->nullable();
             $table->softDeletes();
